193f40da2d86d0d76a5cf6bde24ac177463f2536
erikoenuma <koenuma.eri@gmail.com>
2022-04-22 10:13:26 +0900

    add s3

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.17.0"
 
 set :application, "blog_app"
 set :repo_url, "https://github.com/erikoenuma/AWAKE_task14_aws_deploy"
-set :linked_files, %w{config/secrets.yml}
+set :linked_files, %w{config/secrets.yml .env}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
 set :keep_releases, 5
 set :rbenv_ruby, '3.0.1'
